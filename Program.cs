6959b757b6383885e3d8b76179753c3a26efd015
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-11 11:50:39 +0400

    " My first homework in Vizual Studio "

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CommSchool__1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello User");
+            Console.WriteLine("please enter your name ");
+            Console.ReadLine();
+            Console.WriteLine("please enter your last name ");
+            Console.ReadLine();
+            Console.WriteLine("thansk");
+
+        } 
+    }
+}
