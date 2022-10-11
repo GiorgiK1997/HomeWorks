c94be96b9d399a83bd56e87dd5f03552e0b38696
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-11 17:50:59 +0400

    First change

---
+++ CommSchool_ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CommSchool__1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello User");
+            Console.WriteLine("please enter your name ");
+            Console.ReadLine();
+            Console.WriteLine("please enter your last name ");
+            Console.ReadLine();
+            Console.WriteLine("please enter your ID");
+            Console.ReadLine();
+            Console.WriteLine("thansk");
+
+        } 
+    }
+}
