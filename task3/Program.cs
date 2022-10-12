e7e44598171e9f029bb298030bf7c309c35c2456
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-13 03:21:13 +0400

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i * number);
+            }
+
+
+        }
+    }
+}
