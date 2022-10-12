c8072f3398d02d8e34d2a8624e40a73960723506
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-13 02:03:04 +0400

    Add files via upload
    Week_5 Task2

---
+++ tassk 2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace tassk_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Console.WriteLine("Enter the first number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Choose option");
+            Console.WriteLine(" + ");
+            Console.WriteLine(" / ");
+            Console.WriteLine(" - ");
+            Console.WriteLine(" * ");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+
+                case "-":
+                    if (num2 > num1)
+                    {
+                        result = num2 - num1;
+                    }
+                    else
+                    {
+                        result = num1 - num2;
+                    }
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+
+                case "/":
+                    if (num2 > num1)
+                    {
+                        if (num1 == 0)
+                        {
+                            Console.WriteLine("Cannot be divided by zero");
+                        }
+                        else
+                        {
+                            result = num2 / num1;
+                        }
+                    }
+                    if (num1 > num2)
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Cannot be divided by zero");
+                        }
+                        else
+                        {
+                            result = num1 / num2;
+                        }
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
