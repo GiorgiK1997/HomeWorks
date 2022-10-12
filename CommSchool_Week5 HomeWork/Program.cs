725bf0853ac1692d7c5620f3fd61e3e1f17bc5e2
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-13 02:00:52 +0400

    Add files via upload
    week_5 HomeWork

---
+++ CommSchool_Week5 HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CommSchool_Week5_HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int x = int.Parse(Console.ReadLine());
+            int result = x % 5;
+            Console.WriteLine(result);
+
+            string answer = result > 0 ? "no" : "yes";
+            Console.WriteLine(answer);
+        }
+    }
+}
