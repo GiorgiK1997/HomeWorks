cf4a3a89f7ef5fd2ca138d4017d2bee6c9ead40c
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-13 03:24:50 +0400

    task4

---
+++ CommSchool_ 1/CommSchool_ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CommSchool__1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Enter First Number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"X is {x} and Y is {y}");
+            int z = x;
+            x = y;
+            y = z;
+
+            Console.WriteLine($"X is {x} and Y is {y}");
+        }
+    }
+}
