ab95f9e48b0727f4e1c395127a2d9504459a884d
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-16 21:58:48 +0400

    Week_6_Task_2
    Week_6_Task_2

---
+++ week_6 _task_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+
+namespace week_6__task_2
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listNumber = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                int randNumber = random.Next(10);
+                listNumber.Add(randNumber);
+                Console.WriteLine(randNumber);
+                if(randNumber % 2 == 0)
+                {
+                    Console.WriteLine(randNumber);
+                }
+            }
+
+
+            Console.WriteLine("----------- GROUPS ---------------------");
+
+            var groupedNumbers = listNumber.GroupBy(x => x);
+            foreach (var item in groupedNumbers)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine($".......... {item.Key}  .........{item.Count()}........{item.Sum()}");
+
+
+            }
+
+
+        }
+
+
+
+    }
+}
