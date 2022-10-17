ae9f688ed5e87de3869bc7b7e73fdc5aac7dff8d
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-18 00:00:09 +0400

    week_6_Task_3
    week_6_Task_3

---
+++ Week_6__Task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Week_6__Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Num = new int[10];
+            Num[0] = 4;
+            Num[1] = 7;
+            Num[2] = 8;
+            Num[3] = 6;
+            Num[4] = 5;
+            Num[5] = 10;
+            Num[6] = 9;
+            Num[7] = 1;
+            Num[8] = 2;
+            Num[9] = 3;
+                
+
+            var orderBy = Num.OrderByDescending(x => x);
+            foreach (var item in orderBy . Take(3))
+            {
+                Console.WriteLine(item);
+              
+                
+            }
+
+           
+        }
+    }
+}
