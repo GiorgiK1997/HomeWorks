ca3f30e35a66ee0e5fc4fcc693ddacf5d83618e1
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-21 02:45:25 +0400

    Add files via upload

---
+++ HomeWork _7_Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HomeWork__7_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string word = Console.ReadLine();
+            bool result = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != word[0])
+                {
+                    result = false;
+                }
+                
+            }
+            if(result == false)
+            {
+                Console.WriteLine(" you lost");
+            }
+            else
+            {
+                Console.WriteLine(" you win jakpot");
+            }
+           
+            
+
+        }
+    }
+}
