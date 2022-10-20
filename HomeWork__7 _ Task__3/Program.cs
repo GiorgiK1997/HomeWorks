70487a8b76d9329e65a954690e8852ae91376924
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-21 02:43:05 +0400

    Add files via upload

---
+++ HomeWork__7 _ Task__3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeWork__7___Tas;
+
+    using System.Linq;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter The number 1");
+            int number = int.Parse(Console.ReadLine());
+            List<string> words = new List<string>();
+            words.Add("girogi");
+            words.Add("asasaxs");
+            words.Add("ggiooffd");
+            words.Add("weewwwewewewa");
+            words.Add("hermaqw");
+            words.Add("awerlp");
+            foreach (var item in words.Where(x => x.Length == number))
+            {
+                Console.WriteLine(item);
+            }         
+        }
+    }
