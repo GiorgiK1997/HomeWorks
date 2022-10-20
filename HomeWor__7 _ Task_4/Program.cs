7f3dd025c07ce4edb2a7b06bef3ccb6f27c98cbb
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-10-21 02:41:37 +0400

    HomeWork_7 _task4
    HomeWork_7 _Task4

---
+++ HomeWor__7 _ Task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HomeWork__7___Tas;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        int[] weekDays = new int[6];
+        weekDays[0] = 7;
+        weekDays[1] = 7;
+        weekDays[2] = 5;
+        weekDays[3] = 6;
+        weekDays[4] = 7;
+        weekDays[5] = 7;
+        int result = 0;
+        for (int i = 1; i < weekDays.Length; i++)
+        {
+            if (weekDays[i] > weekDays[i-1])
+            {
+                result  = result + 1;
+                
+            }
+            
+        }
+        Console.WriteLine(result);
+        
+            
+       
+      
+    }
+} 
+
+
