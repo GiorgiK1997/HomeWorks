fccce50bae269e5ae39c8f7efa03852f5fbde264
GiorgiK1997 <115031781+GiorgiK1997@users.noreply.github.com>
2022-11-04 02:52:58 +0400

    Add files via upload

---
+++ HomeWork11/HomeWork11_1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HomeWork11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = @"\GiorgiRepositories\HomeWork11\My file\CommSchool.txt";
+            Console.WriteLine("Enter number line");
+            var   line = int.Parse(Console.ReadLine());
+            var lastLine = "";
+            for (int i = 0; i < line; i++)
+            {
+                var writeText = Console.ReadLine();
+                lastLine = writeText;
+
+            }
+            File.AppendAllText(inputFile, lastLine );
+            StreamReader writer = File.OpenText(inputFile);
+            Console.WriteLine(writer.ReadToEnd());
+           
+
+        }
+    }
+}
